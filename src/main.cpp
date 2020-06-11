863349f0ceaf18af5f7890a6e2ef3e415a4b4768
Matthieu Court <matthieu.court@gmail.com>
2020-06-11 00:57:26 -0400

    👶 commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Input latin phrase: " << endl;
+    string text;
+    cin >> text;
+    cout << "Your phrase is: " << text << endl;
+    return 0;
+}
