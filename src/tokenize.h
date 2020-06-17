9352562f610e3e551b135bb478cd0c8f0391aec2
Matthieu Court <matthieu.court@gmail.com>
2020-06-17 03:29:36 -0400

    added sentence and contextual token gists

--- src/tokenize.h
+++ src/tokenize.h
@@ -5,6 +5,32 @@
 #include <vector>
 using namespace std;
 
+class contextualToken {
+    int index;
+    string val;
+    string tag;
+    vector<int> tailIndices;
+    public:
+        void set_values (int, string, string, vector<int>);
+};
+
+void contextualToken::set_values (int x, string y, string z, vector<int> a) {
+  index = x;
+  val = y;
+  tag = z;
+  tailIndices = a;
+}
+
+class sentence {
+    vector<contextualToken> tokens;
+    public:
+        void add_token (contextualToken);
+};
+
+void sentence::add_token (contextualToken token) {
+    tokens.push_back(token);
+}
+
 vector<string> getSentences(string str) {
     vector<string> sentences;
     size_t found = str.find_first_of(".?!"), start = 0;
@@ -26,7 +52,13 @@ vector<string> getTokens(string str) {
     string temp;
     // latin tokenization just from whitespace ' '.
     while(getline(seek, temp, ' ')) {
-        tokens.push_back(temp);
+        if(temp.back() == '.' || temp.back() == '?' || temp.back() == '!') {
+            temp.pop_back();
+            tokens.push_back(temp);
+        }
+        else {
+            tokens.push_back(temp);
+        }
     }
     // logging ...
     for(int i = 0; i < tokens.size(); i++) {
